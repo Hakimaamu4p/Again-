dea82d1cca9a38097d40aa447b02e02b8ab2cfbb
Hakimaamu4p <amu02934@gmail.com>
2024-09-28 02:36:53 +0000

    Update Help.js

--- scripts/cmds/Help.js
+++ scripts/cmds/Help.js
@@ -9,7 +9,7 @@ module.exports = {
 	config: {
 		name: "help",
 		version: "1.17",
-		author: "NTKhang", // orginal author Kshitiz
+		author: "NTKhang", // Modified by HEAvEN
 		countDown: 5,
 		role: 0,
 		shortDescription: {
@@ -59,7 +59,7 @@ module.exports = {
 			});
 
 			const totalCommands = commands.size;
-			msg += `\n𝗖𝘂𝗿𝗿𝗲𝗻𝘁𝗹𝘆, 𝘁𝗵𝗲 𝗯𝗼𝘁 𝗵𝗮𝘀 ${totalCommands} 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝘁𝗵𝗮𝘁 𝗰𝗮𝗻 𝗯𝗲 𝘂𝘀𝗲𝗱\n`;
+			msg += `\n𝗖𝘂𝗿𝗿𝗲𝗻𝘁𝗹𝘆, 𝗛𝗲𝗮𝘃𝗲𝗻 𝗯𝗼𝘁 𝗵𝗮𝘀 ${totalCommands} 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝘁𝗵𝗮𝘁 𝗰𝗮𝗻 𝗯𝗲 𝘂𝘀𝗲𝗱\n`;
 			msg += `𝗧𝘆𝗽𝗲 ${prefix} 𝗵𝗲𝗹𝗽 𝗰𝗺𝗱𝗡𝗮𝗺𝗲 𝘁𝗼 𝘃𝗶𝗲𝘄 𝘁𝗵𝗲 𝗱𝗲𝘁𝗮𝗶𝗹𝘀 𝗼𝗳 𝘁𝗵𝗮𝘁 𝗰𝗼𝗺𝗺𝗮𝗻𝗱\n`;
 			msg += `HEAVEN BOT BESTBOT 🧘‍♀️`;
 
